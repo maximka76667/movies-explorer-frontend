470783e1e0e65935afbc2ab399dc9ebe77d166b4
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-09-21 14:15:17 +0200

    переместил лого из навигации в хедер

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
+import Logo from '../Logo/Logo';
 
 function Header(props) {
   return (
     <header className="header">
+      <Logo />
       <Navigation />
     </header>
   )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,27 +1,25 @@
 import './Navigation.css'
-import { Link } from 'react-router-dom'
+import { NavLink as Link } from 'react-router-dom'
 import { useLocation } from 'react-router';
-import Logo from '../Logo/Logo';
 
 function Navigation(props) {
   const location = useLocation();
 
   return (
     <div className="nav">
-      <Logo />
       {
         location.pathname !== '/' ?
           (
             <>
               <ul className="nav__links">
-                <li className="nav__item"><Link className="nav__link nav__link_to_movies" to="/movies">Фильмы</Link></li>
-                <li className="nav__item"><Link className="nav__link nav__link_to_saved-movies" to="/saved-movies">Сохранённые фильмы</Link></li>
+                <li className="nav__item"><Link className="nav__link nav__link_to_movies" to="/movies" activeClassName="nav__link_active">Фильмы</Link></li>
+                <li className="nav__item"><Link className="nav__link nav__link_to_saved-movies" to="/saved-movies" activeClassName="nav__link_active">Сохранённые фильмы</Link></li>
               </ul>
               <Link className="nav__link nav__link_to_my-account" to="/profile">Аккаунт</Link>
             </>
           ) :
           (
-            <ul className="nav__links">
+            <ul className="nav__links nav__links_loc_main">
               <li className="nav__item"><Link className="nav__link nav__link_to_signup" to="/signup">Регистрация</Link></li>
               <li className="nav__item"><Link className="nav__link nav__link_to_signin" to="/signin">Войти</Link></li>
             </ul>
