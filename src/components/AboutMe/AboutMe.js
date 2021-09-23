4a361481c0bc6aa7cfd8ea61c4ea7742bb8186dd
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-09-23 19:14:16 +0200

    изменил текст про меня, исправил ширину текста

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,10 +10,8 @@ function AboutMe(props) {
         <div className="about-me__info">
           <h3 className="about-me__name">Максим</h3>
           <h4 className="about-me__short">Фронтенд-разработчик, 17 лет</h4>
-          <p className="about-me__story">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании
-            «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной
-            работы.
+          <p className="about-me__story">Я родился в Астане, сейчас живу в Сантьяго-Де-Компостела. Учусь в колледже разработки приложений.
+            Я люблю слушать музыку, а ещё увлекаюсь видеиграми. В этом году начал кодить.
           </p>
           <ul className="about-me__links">
             <li className="about-me__item"><a className="about-me__link" href="https://www.facebook.com/profile.php?id=100057730898201" target="_blank" rel="noreferrer">Facebook</a></li>
