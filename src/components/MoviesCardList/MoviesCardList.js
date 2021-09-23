14c5e50116b50a909c3fc1ac7effa7684161635c
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-09-23 12:27:59 +0200

    убрал кнопку ещё в /saved-movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import More from '../More/More';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import Preloader from '../Preloader/Preloader';
@@ -11,41 +12,43 @@ function MoviesCardList(props) {
   const [isAllCardsRendered, setIsAllCardsRendered] = React.useState(false);
   // const [countCardsForRender, setCountCardsForRender] = React.useState(3);
 
+  const location = useLocation();
+
   const cardList = [
     {
-      name: '33 слова о дизайне',
+      name: '33 слова о дизайне1',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»2',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»3',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»4',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»5',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»6',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»7',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»8',
       duration: '1ч 17м'
     },
     {
-      name: 'Киноальманах «100 лет дизайна»',
+      name: 'Киноальманах «100 лет дизайна»9',
       duration: '1ч 17м'
     },
   ];
@@ -53,7 +56,9 @@ function MoviesCardList(props) {
   function renderCards() {
     const cardsForRender = [];
 
-    for (let i = 0; i < 3; i++) {
+    const countCardsForRender = location.pathname === "/saved-movies" ? cardList.length : 3;
+
+    for (let i = 0; i < countCardsForRender; i++) {
       const newCardIndex = i + renderedCardList.length
       const newCard = cardList[newCardIndex];
 
