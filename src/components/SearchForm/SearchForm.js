b0c36443da81b225754d033e5186249733fda780
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-09-24 19:29:09 +0200

    добавил required и состояние фокуса к форме поиска фильмов

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import './SearchForm.css';
 function SearchForm(props) {
   return (
     <form className="search-form" onSubmit={props.onSubmit}>
-      <input className="search-form__input" placeholder="Фильм" />
+      <input className="search-form__input" placeholder="Фильм" required />
       <button className="search-form__submit-button" type="submit">Найти</button>
       <FilterCheckbox />
       <hr className="search-form__line" />
