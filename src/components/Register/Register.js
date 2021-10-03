978382cd6e7b877c379a85f514ddb465de72061b
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-10-03 19:47:56 +0200

    добавил валидацию регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,19 +2,107 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import './Register.css';
-import { useInputChange } from '../../utils/useInputChange';
 
 function Register(props) {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  const [input, handleInputChange] = useInputChange();
+  // Validation Constants
+  const [nameError, setNameError] = React.useState('');
+  const [emailError, setEmailError] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState('');
+  const [isNameError, setIsNameError] = React.useState(false);
+  const [isEmailError, setIsEmailError] = React.useState(false);
+  const [isPasswordError, setIsPasswordError] = React.useState(false);
+  const [isSubmitValid, setIsSubmitValid] = React.useState(false);
+
+  // ClassNames
+  const nameInputClassName = (
+    `register__form-input ${isNameError ? 'register__form-input_error' : ''}`
+  );
+
+  const emailInputClassName = (
+    `register__form-input ${isEmailError ? 'register__form-input_error' : ''}`
+  );
+
+  const passwordInputClassName = (
+    `register__form-input ${isPasswordError ? 'register__form-input_error' : ''}`
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    props.onRegister(input);
+    props.onRegister({ name, email, password });
   }
 
+  function handleNameInputChange(e) {
+    setName(e.target.value);
 
+    handleValidation(e);
+  }
+
+  function handleEmailInputChange(e) {
+    setEmail(e.target.value);
+
+    handleValidation(e);
+  }
+
+  function handlePasswordInputChange(e) {
+    setPassword(e.target.value);
+
+    handleValidation(e);
+  }
+
+  function handleValidation(e) {
+    const inputElement = e.target;
+
+    switch (inputElement.name) {
+      case 'name': {
+        if (!inputElement.validity.valid) {
+          setIsNameError(true);
+          setNameError(inputElement.validationMessage);
+          return;
+        }
+        setIsNameError(false);
+        setNameError('');
+        break;
+      }
+      case 'email': {
+        if (!inputElement.validity.valid) {
+          setIsEmailError(true);
+          setEmailError(inputElement.validationMessage);
+          return;
+        }
+        setIsEmailError(false);
+        setEmailError('');
+        break;
+      }
+      case 'password': {
+        if (!inputElement.validity.valid) {
+          setIsPasswordError(true);
+          setPasswordError(inputElement.validationMessage);
+          return;
+        }
+        setIsPasswordError(false);
+        setPasswordError('');
+        break;
+      }
+      default: { }
+    }
+  }
+
+  React.useEffect(() => {
+    if (isNameError || isEmailError || isPasswordError) return setIsSubmitValid(false);
+    return setIsSubmitValid(true);
+  }, [isNameError, isEmailError, isPasswordError, name, email, password])
+
+  React.useEffect(() => {
+    setIsNameError(true);
+    setIsEmailError(true);
+    setIsPasswordError(true);
+    setIsSubmitValid(false);
+  }, [])
 
   return (
     <div className="register">
@@ -24,20 +112,20 @@ function Register(props) {
         <form className="register__form" onSubmit={handleSubmit}>
           <div className="register__form-field">
             <label className="register__form-label">Имя</label>
-            <input className="register__form-input" name="name" type="text" onChange={handleInputChange} required />
-            <p className="register__form-error"></p>
+            <input className={nameInputClassName} name="name" type="text" value={name} onChange={handleNameInputChange} required />
+            <p className="register__form-error">{nameError}</p>
           </div>
           <div className="register__form-field">
             <label className="register__form-label">E-mail</label>
-            <input className="register__form-input" name="email" type="email" onChange={handleInputChange} required />
-            <p className="register__form-error"></p>
+            <input className={emailInputClassName} name="email" type="email" value={email} onChange={handleEmailInputChange} required />
+            <p className="register__form-error">{emailError}</p>
           </div>
           <div className="register__form-field">
             <label className="register__form-label">Пароль</label>
-            <input className="register__form-input" name="password" type="password" onChange={handleInputChange} required />
-            <p className="register__form-error"></p>
+            <input className={passwordInputClassName} name="password" type="password" value={password} onChange={handlePasswordInputChange} required />
+            <p className="register__form-error">{passwordError}</p>
           </div>
-          <button className="register__submit-button" type="submit">Зарегистрироваться</button>
+          <button className="register__submit-button" type="submit" disabled={!isSubmitValid}>Зарегистрироваться</button>
         </form>
         <p className="register__login">Уже зарегистрированы?<Link className="register__login-link" to="/signin">Войти</Link></p>
       </div>
